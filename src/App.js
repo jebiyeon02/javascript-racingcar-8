326363c2f9103c119741573a1d52d55e2e7bea81
jebiyeon02 <jhje5595@naver.com>
2025-10-22 20:43:29 +0900

    feat(input): 사용자로부터 자동차이름을 입력받아 쉼표로 구분하고 공백을 제거

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const names = input.split(",").map((name) => name.replace(" ", ""));
+  }
 }
 
 export default App;
