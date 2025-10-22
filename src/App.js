592140fbca1467d8481403d6be816632383d12f5
jebiyeon02 <jhje5595@naver.com>
2025-10-23 00:00:54 +0900

    feat: 가장 멀리 이동한 자동차의 이동거리를 저장

--- src/App.js
+++ src/App.js
@@ -42,6 +42,17 @@ class App {
     }
   }
 
+  getMaxDistance(cars) {
+    let maxDistance = 0;
+    cars.map((car) => {
+      if (car.forwardDistance > maxDistance) {
+        maxDistance = car.forwardDistance;
+      }
+    });
+
+    return maxDistance;
+  }
+
   async run() {
     const input = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
