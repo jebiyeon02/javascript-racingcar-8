4e845d310c9e123723815a7bbccaeb98c6e811dc
jebiyeon02 <jhje5595@naver.com>
2025-10-22 21:38:38 +0900

    feat: 0~9사이 랜덤으로 추출한 값이 4이상일 때 전진하는 메서드 생성

--- src/Car.js
+++ src/Car.js
@@ -1,6 +1,15 @@
+import { Random } from "@woowacourse/mission-utils";
+
 export default class Car {
   constructor(name) {
     this.name = name;
     this.forwardDistance = 0;
+  }
+
+  forward() {
+    const number = Random.pickNumberInRange(0, 9);
+    if (number >= 4) {
+      this.forwardDistance += 1;
+    }
   }
 }
