9444eb6152f2bf4a911df5be85ce0efd533c7a21
jebiyeon02 <jhje5595@naver.com>
2025-10-23 22:55:31 +0900

    test: readme 기반으로 기능,예외 테스트 코드 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -25,7 +25,7 @@ const getLogSpy = () => {
 };
 
 describe("자동차 경주", () => {
-  test("기능 테스트", async () => {
+  test("기능 테스트1", async () => {
     // given
     const MOVING_FORWARD = 4;
     const STOP = 3;
@@ -46,9 +46,161 @@ describe("자동차 경주", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("기능 테스트2 - 이름에 공백이 존재하는 경우", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["ja mes,pobi", "1"];
+    const logs = ["james : -", "pobi : ", "최종 우승자 : james"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, STOP]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test("기능 테스트3 - 공동 우승자가 존재하는 경우", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const inputs = ["jun,pobi", "1"];
+    const logs = ["jun : -", "pobi : -", "최종 우승자 : jun, pobi"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, MOVING_FORWARD]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test("기능 테스트4 - 시도 횟수가 2회 이상인 경우", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["jun,pobi", "2"];
+    const logs = [
+      "jun : -",
+      "pobi : ",
+      "jun : --",
+      "pobi : -",
+      "최종 우승자 : jun",
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test("예외 테스트1 - 이름이 5자를 초과하는 경우", async () => {
     // given
     const inputs = ["pobi,javaji"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트2 - 빈 문자열을 입력하는 경우", async () => {
+    // given
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트3 - 쉼표의 쓰임이 올바르지 않은 경우(쉼표 사이 빈 값)", async () => {
+    // given
+    const inputs = ["pobi,,woowa"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트4 - 쉼표의 쓰임이 올바르지 않은 경우(쉼표가 문자열의 첫 부분에 등장)", async () => {
+    // given
+    const inputs = [",pobi,woowa"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트5 - 쉼표의 쓰임이 올바르지 않은 경우(쉼표가 문자열의 마지막에 등장)", async () => {
+    // given
+    const inputs = ["pobi,woowa,"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트6 - 2개 이상의 자동차가 같은 이름을 사용하는 경우", async () => {
+    // given
+    const inputs = ["pobi,pobi,"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트7 - 시도횟수가 음수인 경우", async () => {
+    // given
+    const inputs = ["pobi,woowa", "-1"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트8 - 시도횟수가 숫자가 아닌 경우", async () => {
+    // given
+    const inputs = ["pobi,woowa", "@"];
     mockQuestions(inputs);
 
     // when
