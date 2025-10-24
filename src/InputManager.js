942fc62bdb533e38637f93982ba9e236172c3df8
jebiyeon02 <jhje5595@naver.com>
2025-10-24 20:17:45 +0900

    refactor: App.js에 존재하는 콘솔 입력 관련 함수들을 분리하여 InputManager 클래스 내부에서 관리

---
+++ src/InputManager.js
@@ -0,0 +1,51 @@
+import { Console } from '@woowacourse/mission-utils';
+import { ERROR_MESSAGES } from './constants/errorMessages.js';
+import { CONSTANTS } from './constants/constants.js';
+
+export default class InputManager {
+  #names;
+  #playTimes;
+
+  async inputNames() {
+    this.#names = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+  }
+
+  removeWhiteSpaceFromNames() {
+    this.#names = this.#names.split(',').map((name) => name.replace(' ', ''));
+  }
+
+  isValidateNames() {
+    this.#names.map((name) => {
+      if (name.length > CONSTANTS.CAR_NAME_MAX_LENGTH)
+        throw new Error(ERROR_MESSAGES.CAR_NAME_LENGTH_OVER_MAX_LENGTH);
+      if (name.length === 0) throw new Error(ERROR_MESSAGES.CAR_NAME_BLANK);
+      return name;
+    });
+
+    const nameSet = new Set(this.#names);
+    if (this.#names.length !== nameSet.size) {
+      throw new Error(ERROR_MESSAGES.CAR_NAME_SAME);
+    }
+  }
+
+  async inputPlayTimes() {
+    this.#playTimes =
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  }
+
+  isValidatePlayTimes() {
+    if (isNaN(this.#playTimes) || this.#playTimes < 0) {
+      throw new Error(ERROR_MESSAGES.INVALID_PLAY_TIMES);
+    }
+  }
+
+  getNames() {
+    return this.#names;
+  }
+
+  getPlayTimes() {
+    return this.#playTimes;
+  }
+}
