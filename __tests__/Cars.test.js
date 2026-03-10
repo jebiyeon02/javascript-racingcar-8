75abeacca9c9ea435e90a13b7a36d766effcd3ef
jebiyeon02 <jhje5595@naver.com>
2026-03-10 15:43:25 +0900

    test: 5대의 자동차에 중복 이름이 존재하면 예외 발생 테스트 작성.

--- __tests__/Cars.test.js
+++ __tests__/Cars.test.js
@@ -1,4 +1,5 @@
 import App from "../src/App";
+import { ERROR_MESSAGE } from "../src/constants";
 
 describe("자동차 리스트 테스트", () => {
   test("[기능] 5개의 이름을 받아, 5개의 자동차를 생성해야 한다.", () => {
@@ -7,5 +8,13 @@ describe("자동차 리스트 테스트", () => {
 
     // when & then
     expect(App.getCars(names).length).toEqual(5);
+  });
+
+  test("[예외] 5대의 자동차 중 중복 이름이 존재하면 예외가 발생한다.", () => {
+    // given
+    const names = ["dium", "dium", "dol", "hyun", "any"];
+
+    // when & then
+    expect(() => App.getCars(names)).toThrow(ERROR_MESSAGE.PREFIX);
   });
 });
