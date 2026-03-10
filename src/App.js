5cc7f1f4788124e53e585ed5b771222362d84954
jebiyeon02 <jhje5595@naver.com>
2026-03-10 15:43:51 +0900

    feat: 5대의 자동차에 중복이름 존재 시 예외 발생

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Car from "./Car";
+import { ERROR_MESSAGE } from "./constants";
 
 class App {
   async run() {}
 
   static getCars(carNames) {
+    if (carNames.length !== new Set(carNames).size) {
+      throw new Error(ERROR_MESSAGE.PREFIX);
+    }
     return carNames.map((name) => new Car(name));
   }
 }
