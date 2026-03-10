d6ff2d62889b187c92229737cd8504aa72072797
jebiyeon02 <jhje5595@naver.com>
2026-03-10 16:50:28 +0900

    feat: 시도 횟수가 양의 정수가 아니면 예외 발생

--- src/App.js
+++ src/App.js
@@ -10,6 +10,18 @@ class App {
     }
     return carNames.map((name) => new Car(name));
   }
+
+  static tryAllCarsMoveForward(cars, tryCount) {
+    for (let i = 0; i < tryCount; i++) {
+      cars.forEach((car) => car.moveForward(4));
+    }
+  }
+
+  static validateTryCount(tryCount) {
+    if (tryCount < 1) {
+      throw new Error(ERROR_MESSAGE.PREFIX);
+    }
+  }
 }
 
 export default App;
